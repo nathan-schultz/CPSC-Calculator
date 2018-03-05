161412e88879990de78894876c8d9a927031cca1
nathan-schultz <36967715+nathan-schultz@users.noreply.github.com>
2018-03-05 01:28:16 -0700

    Add files via upload

---
+++ src/MathFunctions.java
@@ -0,0 +1,20 @@
+
+public class MathFunctions {
+	// square root, percentage, squared, exponent function, factorial(?), log (?)
+	
+	public double mult(double num1, double num2) {
+		return num1 * num2;
+	}
+	
+	public double add(double num1, double num2) {
+		return num1 + num2;
+	}
+	
+	public double divide(double num1, double num2) {
+		return num1 / num2;
+	}
+	
+	public double subt(double num1, double num2) {
+		return num1 - num2;
+	}
+}
